6c36ae24de3270361f086a22c566cc577ff30351
Eric Engoron <eric.engoron@gmail.com>
2017-07-06 15:15:46 -0400

    added CSV of unattached calls

---
+++ scripts/csv_of_unattached_calls.py
@@ -0,0 +1,41 @@
+import sys
+import argparse
+import logging
+from closeio_api import Client as CloseIO_API
+import csv
+
+
+parser = argparse.ArgumentParser(description='Create a CSV of unattached calls for an organization')
+
+parser.add_argument('--api-key', '-k', required=True, help='API Key')
+parser.add_argument('--development', '-d', action='store_true',
+                    help='Use a development (testing) server rather than production.')
+
+args = parser.parse_args()
+
+
+api = CloseIO_API(args.api_key, development=args.development)
+
+offset = 0
+has_more = True
+activities = []
+
+while has_more: 
+	resp = api.get('activity/call', params={'_skip':offset})
+
+	calls = resp['data']
+	for call in calls:
+		if call['lead_id'] == None and call['contact_id'] == None:
+			activities.append(call)
+
+	offset+=len(calls)
+	has_more = resp['has_more']
+
+f = open('unattached_calls.csv', 'wt')
+try:
+    writer = csv.writer(f)
+    writer.writerow( ('Date', 'User', 'Customer Phone', 'Direction', 'Duration', 'voicemail_url') )
+    for a in activities:
+        writer.writerow(('%s' % a['date_created'], '%s' % a['created_by_name'], '%s' % a['remote_phone'], '%s' % a['direction'], '%s' % a ['duration'], '%s' % a['recording_url'])) 
+finally:
+    f.close()
